2372f11aadbad7bb408dedb90d2711db5f886a2b
Mike O'Hara <mike.ohara@bluespurs.com>
2018-05-25 10:40:57 -0300

    Continuing

--- js/main.js
+++ js/main.js
@@ -1,1 +1,87 @@
+
+// lesson 2/1
 // alert('Your Javascript is linked!');
+
+// Lesson 2/6
+
+//console.log('IM A STRING')
+//
+//console.log('IMA' + 'STRING')
+//
+//console.log(42)
+//
+//console.log(21 + 21)
+//
+//console.log(43 - 1)
+//
+//console.log(21 * 2)
+//
+//console.log(42 / 2)
+//
+//console.log(true)
+
+/* 02/07
+// CONSTS cannot be reassigned once defined.
+const nowApples = 4
+
+console.log(nowApples)
+
+const gettingApples = 27
+
+const totalApples = nowApples + gettingApples
+
+console.log(totalApples)
+
+// LETs can be reassigned
+let iWantMoreApples = 2
+const changedMind = 6
+
+iWantMoreApples = changedMind
+
+console.log(iWantMoreApples)
+
+gettingApples = changedMind
+
+*/
+
+// functions
+
+/*
+function logger(code) {
+    console.log(code);
+}
+
+logger('200, Success');
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+};
+
+add(10, 12)
+
+function multiply(numm1, numm2) {
+    console.log(numm1 * numm2);
+}
+
+multiply(2, 10);
+
+*/
+
+// arrow functions
+
+//const zeroArgs = () => { console.log(10) }
+//
+//const zeroWithUnder = _ => { console.log(10) }
+//
+//const oneArg = arg1 => { console.log(arg1 + 5) }
+
+console.log(document)
+
+//console.log(document.querySelector('[data-type="master"]'))
+//console.log(document.querySelector('li'))
+//console.log(document.querySelector('.badboy'))
+
+
+const li = document.querySelector('li')
+li.classList.add('red')
+li.classList.remove('luke')
